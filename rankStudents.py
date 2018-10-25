3f2c2759f3f1c0d4495b8b7df9b06158c146a2ae
SaaR1610 <41052136+SaaR1610@users.noreply.github.com>
2018-10-25 19:34:27 +0530

    Update rankStudents.py

--- rankStudents.py
+++ rankStudents.py
@@ -1,3 +1,4 @@
+This is Coding
 # Read the names and marks of at least 3 students
 # Rank the top three students with highest marks
 # Give cash rewards. $500 for first rank, $300 for second rank, $100 for third rank. Value cannot be modified
